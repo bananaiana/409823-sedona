c8ca40f699f106bb78369b3de8f45bebffd98210
bananaiana <PrivetyaIana@gmail.com>
2017-09-14 18:31:25 +0300

    все ближе к финалу

--- project-17789/js/script.js
+++
@@ -1,6 +0,0 @@
-var formButton = document.querySelector('.form-search');
-var form = document.querySelector('.form');
-
-formButton.addEventListener('click', function () {
-    form.classList.toggle('active');
-});
